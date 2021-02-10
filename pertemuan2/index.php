28b02c93f061e49c4a35908adf5e9f9bd2417d1b
Hazacky <zackyrr7@gmail.com>
2021-02-10 11:57:22 +0700

    pratikum 2 - sintaks dasar php

---
+++ pertemuan2/index.php
@@ -0,0 +1,62 @@
+<?php
+// pertemuan 2- PHP dasar
+//Sintaks PHP
+
+
+//Standar output
+//echo, print
+//print_r
+//var_dump
+
+
+
+//penulisan sintaks PHP
+//1. PHP di dalam HTMl
+//2. Html di dalam PHP
+
+
+//variabel dan tipe Data
+//Variabel
+//tidak boleh diawali dengan angka,tapi boleh mengandung angka
+
+// $nama = "Hazacky";
+// echo "Halo,nama saya '$nama'";
+
+//operator
+//aritmatika
+//+-*/%
+
+//penggabung string/ concatenation/ concat
+//.
+
+// $nama_depan = "hazacky";
+// $nama_belakang = "Ramadhan";
+// echo $nama_depan . " " . $nama_belakang;
+
+
+//Assigment
+//=, +=,-=, *=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+
+// $nama ="Hazacky";
+// $nama .= " ";
+// $nama .= "Ramadhan" ;
+// echo $nama;
+
+//perbandingan
+//<, >, <=, >=, ==, !=
+// var_dump(1 = "1");
+
+//identitas
+// === ,!==
+// var_dump(1 ==="1");
+
+//logika
+// &&, ||, !
+$x = 30;
+var_dump ($x < 20 || $x % 2 == 0);
+
+
+?>
