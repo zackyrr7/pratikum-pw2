78b104885d25b8578059712addbb2a6a5bc4c523
Hazacky <zackyrr7@gmail.com>
2021-02-23 16:13:59 +0700

    pratikum 6 - parsing template frontend

---
+++ Dasar/paragraph.php
@@ -0,0 +1,16 @@
+<?php
+function paragrafSederhana(){
+  echo "
+  <h1>Judul Berita</h1>
+<h2>Subjudul</h2>
+<p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec feugiat in fermentum posuere urna nec tincidunt praesent. Morbi non arcu risus quis varius quam. Imperdiet massa tincidunt nunc pulvinar sapien. Quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed. Rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui. Ipsum faucibus vitae aliquet nec. In fermentum posuere urna nec tincidunt praesent semper feugiat. Vel fringilla est ullamcorper eget nulla facilisi. At ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Ac turpis egestas sed tempus. Tortor consequat id porta nibh venenatis cras sed felis eget. Facilisis volutpat est velit egestas dui id.
+    </p>
+    <p>
+      Ultricies leo integer malesuada nunc. Convallis a cras semper auctor neque. Non enim praesent elementum facilisis leo vel fringilla. Et leo duis ut diam quam nulla porttitor massa. Leo in vitae turpis massa sed elementum tempus egestas. Neque sodales ut etiam sit amet nisl purus in. Elementum tempus egestas sed sed risus. Commodo ullamcorper a lacus vestibulum. Eget gravida cum sociis natoque penatibus et magnis. Leo duis ut diam quam nulla porttitor massa. Ullamcorper a lacus vestibulum sed arcu non.
+    </p>
+  ";
+}
+
+?>
+
---
+++ frontend/about.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Blog Post - Start Bootstrap Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/blog-post.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <?php require_once('partial/navigasi.php') ?>
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <div class="row">
+
+      <!-- Post Content Column -->
+      <div class="col-lg-8">
+
+        <!-- Title -->
+        <h1 class="mt-4">tentang Kami</h1>
+
+        <!-- Author -->
+        <p class="lead">
+          by
+          <a href="#">Siapa kita</a>
+        </p>
+
+        <hr>
+
+        <!-- Date/Time -->
+        <p>Posted on January 1, 2019 at 12:00 PM</p>
+
+        <hr>
+
+        <!-- Preview Image -->
+        <img class="img-fluid rounded" src="http://placehold.it/900x300" alt="">
+
+        <hr>
+
+        <!-- Post Content -->
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!</p>
+
+        
+
+     
+
+      </div>
+
+      <!-- Sidebar Widgets Column -->
+        <?php require_once('partial/sidebar.php') ?>
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <?php require_once('partial/footer.php') ?>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
