6d31ffe192e49878ed82a68379ddfb7ed8a42a94
Hazacky <zackyrr7@gmail.com>
2021-04-01 16:41:08 +0700

    backup

--- cybercampus/resources/views/site/layanan.blade.php
+++ cybercampus/resources/views/site/layanan.blade.php
@@ -4,16 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Layanan</title>
+    <title>kontak</title>
 </head>
 <body>
-    <h1>Layanan</h1>
-    <p>Layanan program studi sistem informasi meliputi: 
-    <ul>
-    <li>pendidikan dan pengajaran</li>
-    <li>Bakti pada Masyarakat</li>
-    <li>Penelitian</li>
-    </ul>
-    </p>
+    <h1>Kontak kami</h1>
+    <form>
+  <label for="nama">Nama :</label>
+  <input type="text" id="nama" name="nama"><br> <br>
+  <label for="email">Email:</label>
+  <input type="text" id="email" name="email"><br> <br>
+  <label for="Tanggapan">Tanggapan:</label> <br>
+  <textarea input width = 300px type="text"  id="Tanggapan" name="Tanggapan"> </textarea><br> <br>
+  <input type = "button" name = "kirim" value = "kirim">
+  
+</form>
 </body>
 </html>
